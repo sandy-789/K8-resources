82a1720475e081ac74bccb8b46c75cf4dff1c227
sandy-789 <sandeepchary125@gmail.com>
2024-06-17 22:19:15 +0530

    E

--- provider.tf
+++ provider.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "daws76s-remote-state"
-    key    = "eksctl"
-    region = "us-east-1"
-    dynamodb_table = "daws76s-locking"
-  }
+#   backend "s3" {
+#     bucket = "daws76s-remote-state"
+#     key    = "eksctl"
+#     region = "us-east-1"
+#     dynamodb_table = "daws76s-locking"
+#   }
 }
 
 provider "aws" {
